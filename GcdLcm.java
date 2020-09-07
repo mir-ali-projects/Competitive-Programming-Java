2515496e076d37b251cfe7bd20412d0fbaa799e1
Mir Ali <66625456+mir-ali-projects@users.noreply.github.com>
2020-09-07 17:57:39 +0530

    Create GcdLcm.java

---
+++ GcdLcm.java
@@ -0,0 +1,22 @@
+import java.util.*;
+    
+    public class Main{
+    
+    public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int num1, num2, copyn1 ,copyn2,rem=1;
+      num1 = scn.nextInt();
+      num2 = scn.nextInt();
+      copyn1 = num1;
+      copyn2 = num2;
+      while(copyn1%copyn2!=0)
+      {
+          rem = copyn1%copyn2;
+          copyn1 = copyn2;
+          copyn2 = rem;
+      }
+        System.out.println(rem);
+        System.out.println((num1*num2)/rem);
+      
+     }
+    }
